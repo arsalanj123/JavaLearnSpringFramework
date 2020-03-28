3e14c83e48ee1f26e034216186a55312389927f6
Arsalan Jawaid <arsalan.jawaidstu@gmail.com>
2020-03-28 17:15:15 +0400

    First Commit - Working Code

---
+++ src/main/java/io/javabrains/springbootdevelopment/topics/TopicService.java
@@ -0,0 +1,43 @@
+package io.javabrains.springbootdevelopment.topics;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+
+    private List<Topic> topics = new ArrayList<>(Arrays.asList (
+                new Topic("Spring", "Spring framework", "Spring framework description"),
+                new Topic("Java", "Core Java", "Core Java description"),
+                new Topic("Javascript", "Javascript", "Javascript description")
+        ));
+
+    public List<Topic> getAllTopics() {
+        return topics;
+    }
+
+    public Topic getTopic(String id){
+        return topics.stream().filter(topic -> topic.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    public void updateTopic(String id, Topic topic) {
+        for (int i = 0; i <  topics.size(); i++) {
+            Topic t = topics.get(i);
+            if (t.getId().equals(id)){
+                topics.set(i, topic);
+                return;
+            }
+        }
+    }
+
+    public void deleteTopic(String id) {
+        topics.removeIf(topic -> topic.getId().equals(id));
+    }
+}
